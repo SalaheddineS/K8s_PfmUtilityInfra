0ec5164ec32b05bf2e36abe66c4384e312b3831d
salaheddineS <SALAHeddinemorchid1@gmail.com>
2023-11-18 00:19:00 +0100

    [TEST] Triggering pipeline

--- pfm_services/helloworld_ms_project/helloworld_ms/Classes/Calculator.cs
+++ pfm_services/helloworld_ms_project/helloworld_ms/Classes/Calculator.cs
@@ -18,7 +18,7 @@ public class Calculator
 
     public int Divide(int a, int b)
     {
-        if (b ==0)
+        if (b == 0)
         {
             throw new ArgumentException("Cannot divide by zero");
         }
